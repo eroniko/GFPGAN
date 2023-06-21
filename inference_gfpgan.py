fc4d4aa5cf3213c78a8c42ae9b3c5a18f29da37a
eroniko <46302653+eroniko@users.noreply.github.com>
2023-06-21 18:36:24 +0300

    Update inference_gfpgan.py

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -1,3 +1,5 @@
+#python3 inference_gfpgan.py -i /root/imgai/inputs/3.jpg -o /root/imgai/results/ -v 1.4 -s 2
+
 import argparse
 import cv2
 import glob
@@ -123,51 +125,54 @@ def main():
         bg_upsampler=bg_upsampler)
 
     # ------------------------ restore ------------------------
-    for img_path in img_list:
-        # read image
-        img_name = os.path.basename(img_path)
+#    for img_path in img_list:
+#        # read image
+#        img_name = os.path.basename(img_path)
+#        print(f'Processing {img_name} ...')
+#        basename, ext = os.path.splitext(img_name)
+#        input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+#
+#        # restore faces and background if necessary
+#        cropped_faces, restored_faces, restored_img = restorer.enhance(
+#            input_img,
+#            has_aligned=args.aligned,
+#            only_center_face=args.only_center_face,
+#            paste_back=True,
+#            weight=args.weight)
+#
+#        # save faces
+#        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
+#            # save cropped face
+#            save_crop_path = os.path.join(args.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
+#            imwrite(cropped_face, save_crop_path)
+#            # save restored face
+#            if args.suffix is not None:
+#                save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
+#            else:
+#                save_face_name = f'{basename}_{idx:02d}.png'
+#            save_restore_path = os.path.join(args.output, 'restored_faces', save_face_name)
+#            imwrite(restored_face, save_restore_path)
+#            # save comparison image
+#            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
+#            imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
+
+        img_name = os.path.basename(img_list)
         print(f'Processing {img_name} ...')
         basename, ext = os.path.splitext(img_name)
-        input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-
-        # restore faces and background if necessary
-        cropped_faces, restored_faces, restored_img = restorer.enhance(
-            input_img,
-            has_aligned=args.aligned,
-            only_center_face=args.only_center_face,
-            paste_back=True,
-            weight=args.weight)
-
-        # save faces
-        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
-            # save cropped face
-            save_crop_path = os.path.join(args.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
-            imwrite(cropped_face, save_crop_path)
-            # save restored face
-            if args.suffix is not None:
-                save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
-            else:
-                save_face_name = f'{basename}_{idx:02d}.png'
-            save_restore_path = os.path.join(args.output, 'restored_faces', save_face_name)
-            imwrite(restored_face, save_restore_path)
-            # save comparison image
-            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
-            imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
+        input_img = cv2.imread(img_list, cv2.IMREAD_COLOR)
 
         # save restored img
         if restored_img is not None:
-            if args.ext == 'auto':
-                extension = ext[1:]
-            else:
-                extension = args.ext
-
+            extension = 'png'
+        
             if args.suffix is not None:
-                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}_{args.suffix}.{extension}')
+                save_restore_path = f'{os.path.splitext(args.output)[0]}_{args.suffix}.{extension}'
             else:
-                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}.{extension}')
+                save_restore_path = f'{args.output}.{extension}'
             imwrite(restored_img, save_restore_path)
 
-    print(f'Results are in the [{args.output}] folder.')
+
+        print(f'Result is in the [{args.output}] file.')
 
 
 if __name__ == '__main__':
